c150e62db87a660f2b8a872e0f521d2489c1884e
Noah Ney <noah.ney@epitech.eu>
2024-02-07 19:51:13 +0100

    [ADD] about me section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,20 @@ function App() {
   return <>
   <div className="main">
     <HeaderComp />
+    <div className="body-container">
+      <div className="left">
+        <section className="about-me">
+          <article>
+            <h2>About Me</h2>
+            <p>Hi, I'm Noah Ney, a first year Epitech student.
+              I am passionate about computer science and I am always looking for new challenges.
+              I am currently learning C, HTML, CSS, Javascript and more precisely React.</p>
+          </article>
+        </section>
+        <section className="skills">
+        </section>
+      </div>
+    </div>
   </div>
   </>;
 }
